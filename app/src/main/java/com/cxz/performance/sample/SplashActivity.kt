be75c6b4b3835e6a15b9b9bdd775fee1fead2f85
chenxz <563859095@qq.com>
2021-06-04 10:19:38 +0800

    commit

---
+++ app/src/main/java/com/cxz/performance/sample/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.cxz.performance.sample
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            finish()
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        }, 3000)
+
+    }
+}
